19c686cf0b38f10bcc8132e7c973b780c4b43de9
Arseny Radushin <radushin.arseny@yandex.ru>
2018-12-01 20:35:49 +0300

    start

--- run.py
+++ run.py
@@ -9,6 +9,31 @@ db_query = DbQuery()
 bot = telebot.TeleBot(config.token)
 logging.basicConfig(filename="sample.log", level=logging.INFO)
 
+@bot.message_handler(commands=['start'])
+def insert_into_a_db(message):
+    query = """SELECT auth
+	        FROM public."user"
+            WHERE id={};"""
+    query_result=db_query.execute_query(query.format(message.chat.id))
+    if len(query_result.value)<1:
+        query ="""INSERT INTO public."user"
+        (id, full_name_telegram,stage,auth)
+        VALUES ({},'{}',{}, False);"""
+        name = 'Unknown user'
+        if message.chat.first_name:
+            name = str(message.chat.first_name)
+        if message.chat.last_name:
+            name = name + ' ' + str(message.chat.last_name)
+        query_result=db_query.execute_query(query.format(message.chat.id,name,0),is_dml=True)
+        if (query_result.success):
+            bot.send_message(message.chat.id, "So, tell us the key")
+    else:
+        if not query_result.value[0][0]:
+            bot.send_message(message.chat.id, "Tell us the key")
+        else:
+            bot.send_message(message.chat.id, "You are already logged in")
+    pass
+
 @bot.message_handler(content_types='text')
 def default_answer(message):
     bot.send_message(message.chat.id, "You are not authorized")
